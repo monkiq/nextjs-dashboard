fc0cf65fb0b3deeac3df7aa9ec5705a7e8917133
monkiqchen <monkiqchen@tencent.com>
2024-04-07 20:53:02 +0800

    practice: 为LatestInvoices添加Suspense

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -35,6 +35,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
+  console.log('Fetching latest invoices...');
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
